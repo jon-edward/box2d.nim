2fff9a55999442871f5791982adc57769d103617
jon-edward <townsend.jonathan.e@gmail.com>
2024-06-26 22:15:48 -0400

    Add static compilation step

--- src/box2d.nim
+++ src/box2d.nim
@@ -1,9 +1,28 @@
-when defined(useFutharkForBox2D):
-  import futhark, os
+{.passC: "-I ../box2c/include"}
 
-  const box2dRoot = currentSourcePath.parentDir.parentDir / "box2c/include/box2d"
-  echo box2dRoot
+{.passC: "-I ../box2c/extern/glad/include"}
+{.passC: "-I ../box2c/extern/jsmn"}
+{.passC: "-I ../box2c/extern/simde"}
 
-  importc:
-    path box2dRoot
-    "box2d.h"
+import os, macros, strutils
+
+
+macro compileCFiles(sourceDir: static[string]): untyped = 
+  result = newStmtList()
+
+  for file in walkDir(sourceDir):
+    let filePath = file.path
+
+    if not filePath.endsWith(".c"):
+      continue
+
+    var pragma = newNimNode(nnkPragma)
+    pragma.add newColonExpr(newIdentNode("compile"), newStrLitNode(file.path))
+
+    result.add pragma
+  
+  result = quote do:
+    block: `result`
+
+
+compileCFiles("../box2c/src")
