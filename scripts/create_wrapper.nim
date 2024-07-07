87b47b0f70371be4bc44d34ec5797b18befd7698
jon-edward <townsend.jonathan.e@gmail.com>
2024-07-07 00:11:42 -0400

    Add create_wrapper.nim

--- scripts/create_wrapper.nim
+++ scripts/create_wrapper.nim
@@ -18,29 +18,44 @@ const futharkSysPath {.define: "futharkSysPath".} = getClangIncludePath()
 const compilerArg = futharkSysPath.split(":").map(proc (s: string): string = "-I" & s).join(" ")
 
 
-# Box2D API
-importc:
-    compilerArg compilerArg
-    path box2cRoot / "include" / "box2d"
-    outputPath outputDir / "generated_include.nim"
-    "base.h"
-    "box2d.h"
-    "collision.h"
-    "id.h"
-    "math_functions.h"
-    "types.h"
+when defined(createBox2dApiWrapper) or not (defined(createBox2dSrcWrapper) or defined(createBox2dInlineWrapper)):
+    # Box2D API
+    importc:
+        compilerArg compilerArg
+        path box2cRoot / "include" / "box2d"
+        outputPath outputDir / "generated_include.nim"
+        "base.h"
+        "box2d.h"
+        "collision.h"
+        "id.h"
+        "math_functions.h"
+        "types.h"
 
 
-# Included in tests
-importc:
-    compilerArg compilerArg
-    path box2cRoot / "src"
-    sysPath box2cRoot / "jsmn"
-    sysPath box2cRoot / "include"
-    sysPath box2cRoot / "extern" / "simde"
-    sysPath box2cRoot / "extern" / "glad" / "include"
-    outputPath outputDir / "generated_src.nim"
-    "aabb.h"
-    "bitset.h"
-    "ctz.h"
-    "table.h"
+when defined(createBox2dSrcWrapper):
+    # Included in tests
+    importc:
+        compilerArg compilerArg
+        path box2cRoot / "src"
+        sysPath box2cRoot / "jsmn"
+        sysPath box2cRoot / "include"
+        sysPath box2cRoot / "extern" / "simde"
+        sysPath box2cRoot / "extern" / "glad" / "include"
+        outputPath outputDir / "generated_src.nim"
+        "aabb.h"
+        "bitset.h"
+        "ctz.h"
+        "table.h"
+
+
+when defined(createBox2dInlineWrapper):
+    # Inline functions
+    importc:
+        compilerArg compilerArg
+        path box2cRoot.parentDir / "inline"
+        outputPath outputDir / "generated_inline.nim"
+        "math_functions.h"
+        "aabb.h"
+        "bitset.h"
+        "ctz.h"
+        "table.h"
