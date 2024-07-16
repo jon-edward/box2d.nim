0aac31ad955b31e673da3b9d0fc54e7643b810ae
jon-edward <townsend.jonathan.e@gmail.com>
2024-07-15 22:47:17 -0400

    Add TOC and custom style cfg

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,4 @@
 nim doc --index:only --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./src/box2d.nim
 nim md2html --index:only --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./docs/index.md
-
-nim doc --project -d:boot --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./src/box2d.nim
-nim md2html --project -d:boot --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./docs/index.md
+nim doc --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./src/box2d.nim
+nim md2html --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./docs/index.md
