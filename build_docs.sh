4544600455ccdf0605d44456b366e21be216bc19
jon-edward <townsend.jonathan.e@gmail.com>
2024-07-28 19:10:18 -0400

    Fix broken doc links

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,7 @@
-nim doc --index:only --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./src/box2d.nim
-nim md2html --index:only --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./docs/index.md
+cd ./src
 
-nim doc --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./src/box2d.nim
-nim md2html --project --d:nimPreviewSlimSystem --outdir:"./docs/htmldocs" ./docs/index.md
+nim doc --index:only --project --d:nimPreviewSlimSystem --outdir:"../docs/htmldocs" box2d.nim
+nim md2html --index:only --project --d:nimPreviewSlimSystem --outdir:"../docs/htmldocs" ./docs/index.md
+
+nim doc --project --d:nimPreviewSlimSystem --outdir:"../docs/htmldocs" box2d.nim
+nim md2html --project --d:nimPreviewSlimSystem --outdir:"../docs/htmldocs" ../docs/index.md
